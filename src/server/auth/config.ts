600261e8914f544c75ef16b3401c6e69a1967570
jimerik1 <jim.strand@gmail.com>
2024-12-28 22:58:14 +0100

    Added Google Auth

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -7,7 +7,7 @@ import { db } from "~/server/db";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
+ * object and keep type safety..
  *
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
