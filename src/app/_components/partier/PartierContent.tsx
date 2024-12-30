93828cd4731904da8b41360c65d8c468d345f682
jimerik1 <jim.strand@gmail.com>
2024-12-30 11:05:53 +0100

    Prisma migration

--- src/app/_components/partier/PartierContent.tsx
+++ src/app/_components/partier/PartierContent.tsx
@@ -104,7 +104,7 @@ export default function PartiContent({ session }: PartierContentsProps) {
               onClick={() => setSearchOpen(true)}
               className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Søk parti
+              Søk partier
             </button>
           </div>
         </div>
