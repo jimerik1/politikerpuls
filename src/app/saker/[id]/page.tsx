b7eb6015ca2906f4ff5bb99070c380dd16af15cf
jimerik1 <jim.strand@gmail.com>
2025-01-03 22:21:18 +0100

    la sider til egen route try 3

--- src/app/saker/[id]/page.tsx
+++ src/app/saker/[id]/page.tsx
@@ -4,20 +4,17 @@ import DashboardLayout from "../../_components/authorized/DashboardLayout";
 import CaseDetailsContent from "./CaseDetailsContent";
 import { api } from "~/trpc/server";
 
-interface PageProps {
-    params: {
-      id: string;
-    };
-  }
-  
-  export default async function Page({ params }: PageProps) {
+export default async function Page({
+    params,
+  }: {
+    params: { id: string }
+  }) {
     const session = await auth();
     
     if (!session) {
       redirect("/api/auth/signin");
     }
   
-    // Remove await from params.id
     const caseDetails = await api.case.getDetailedCase({ stortingetId: params.id });
   
     if (!caseDetails) {
